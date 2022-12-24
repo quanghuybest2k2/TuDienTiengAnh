91ba04934a63d00b6a682bcd2074b2ced4132ca5
Doan Quang Huy <2015597@dlu.edu.vn>
2022-12-24 09:18:59 +0700

    upload projecr

---
+++ app/src/main/java/com/bignerdranch/android/vocabularyapp/SearchAllFragment.java
@@ -0,0 +1,317 @@
+package com.bignerdranch.android.vocabularyapp;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.speech.RecognizerIntent;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.bignerdranch.android.vocabularyapp.database.DatabaseHelper;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
+
+public class SearchAllFragment extends Fragment {
+
+    private AutoCompleteTextView mAutoTxtSearchAll;
+    private DatabaseHelper mDatabaseHelper;
+    private ImageButton mButtonVoice;
+    private ImageButton mBtnCamera;
+    Button mDialogBtnCamera;
+    Button mDialogBtnGallery;
+    ImageView mPreviewIv;
+    private WordAdapter dataAdapter;
+
+    private static final int REQUEST_CODE_CHANGED_STATUS = 10;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 2;
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+    private static final int IMAGE_PICK_CAMERA_CODE = 1001;
+
+    String cameraPermission[];
+    String storagePermission[];
+
+    Uri image_uri;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_all,container,false);
+        mAutoTxtSearchAll  = view.findViewById(R.id.searchAll);
+        mDatabaseHelper = new DatabaseHelper(getActivity());
+
+        //search the whole vocabulary
+        mAutoTxtSearchAll.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 1) {
+                    mAutoTxtSearchAll.setAdapter(new ArrayAdapter<String>(getActivity(),
+                            android.R.layout.simple_list_item_1, mDatabaseHelper.getEngWord(s.toString())));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Handle item of the list
+        mAutoTxtSearchAll.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String mWord = (String) parent.getItemAtPosition(position);
+                getDescription(mWord);
+            }
+        });
+
+        //event search by voice
+        mButtonVoice = view.findViewById(R.id.button_voice);
+        mButtonVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+
+        });
+
+        //define image view to set image when take a photo
+        mPreviewIv = view.findViewById(R.id.imageIv);
+        //event search by taking a photo
+        mBtnCamera = view.findViewById(R.id.button_camera);
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImageImportDialog();
+            }
+        });
+
+        //camera permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        //storage permission
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        return view;
+    }
+
+    //Handle voice to text
+    private void speak() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.UK);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hãy nói gì đó...");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        }catch (Exception e){
+            Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Show dialog to select camera or gallery
+    private void showImageImportDialog() {
+        Dialog dialogCamera = new Dialog(getContext());
+        dialogCamera.setContentView(R.layout.dialog_choose_camera);
+        dialogCamera.getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg_choose_camera);
+        dialogCamera.show();
+
+        mDialogBtnCamera = (Button) dialogCamera.findViewById(R.id.btn_camera);
+        mDialogBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //camera option click
+                if(!checkCameraPermission()){
+                    //camera permission not allowed, request it
+                    requestCameraPermission();
+                }else{
+                    //permission allowed, take picture
+                    pickCamera();
+                }
+                dialogCamera.dismiss();
+            }
+        });
+
+        mDialogBtnGallery = (Button) dialogCamera.findViewById(R.id.btn_gallery);
+        mDialogBtnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //gallery option click
+                if(!checkStoragePermission()){
+                    //storage permission not allowed, request it
+                    requestStoragePermission();
+                }else{
+                    //permission allowed, take picture
+                    pickGallery();
+                }
+                dialogCamera.dismiss();
+            }
+        });
+    }
+
+    private void pickGallery() {
+        //intent to take image from gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickCamera() {
+        //intent to take image from camera, it will also be save to get high quality image
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "NewPic"); //title of picture
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image To Text"); //description
+        image_uri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(getActivity(), storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(getActivity(), cameraPermission, CAMERA_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission() {
+        //Check camera permission and return the result
+        //In order to get high quality picture we have to save image to external storage first
+        //before interesting to image view that's why storage permission will also be required
+        boolean result = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+
+    }
+
+    //call displayWordActivity class
+    private void getDescription(String word) {
+        Intent intent = new Intent(getActivity(), DisplayWordActivity.class);
+        intent.putExtra("word", word);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                //got image from gallery now crop it
+                CropImage.activity(data.getData())
+                        .setGuidelines(CropImageView.Guidelines.ON) //enable image guidelines
+                        .start(getContext(), SearchAllFragment.this);
+            }
+
+            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                //got image from camera now crop it
+                CropImage.activity(image_uri)
+                        .setGuidelines(CropImageView.Guidelines.ON) //enable image guidelines
+                        .start(getContext(), SearchAllFragment.this);
+            }
+        }
+
+        switch (requestCode) {
+            //case voice to text
+            case REQUEST_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null) {
+                    //get text data from voice intent
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //set to text view
+                    getDescription(result.get(0));
+                }
+                break;
+            }
+
+            //get cropped image
+            case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE: {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    Uri resultUri = result.getUri(); //get image uri
+                    // set image to image view
+                    mPreviewIv.setImageURI(resultUri);
+
+                    //get drawable bitmap for text recognition
+                    BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
+                    Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                    TextRecognizer textRecognizer = new TextRecognizer.Builder(getActivity().getApplicationContext()).build();
+                    if (!textRecognizer.isOperational()) {
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                        SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                        StringBuilder sb = new StringBuilder();
+                        //get text from sb until there is no text
+                        for (int i = 0; i < items.size(); i++) {
+                            TextBlock myItem = items.valueAt(i);
+                            sb.append(myItem.getValue());
+                        }
+                        //call displayWord by text
+                        getDescription(sb.toString());
+                    }
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    //if there any error show it
+                    Exception error = result.getError();
+                    Toast.makeText(getContext(), "" + error, Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+
+            case REQUEST_CODE_CHANGED_STATUS:{
+                Intent i = new Intent(getActivity(), FavoriteActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        }
+    }
+}
